da2fa3feadc871de0eaa0ab491a7226f2025ed06
FlorezZ1025 <sanaraflorez@gmail.com>
2025-05-11 22:39:17 -0500

    endpoint

--- src/utils/db.py
+++ src/utils/db.py
@@ -5,15 +5,15 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-ENDOPOINT_SUP_S3 = os.getenv('ENDPOINT_SUP_S3')
+ENDPOINT_SUP_S3 = os.getenv('ENDPOINT_SUP_S3')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 # BUCKET_NAME = 'evidences-pdfs'
-print(ENDOPOINT_SUP_S3)
+print(ENDPOINT_SUP_S3)
 print(AWS_ACCESS_KEY_ID)
 s3 = boto3.client(
     's3',
-    endpoint_url=ENDOPOINT_SUP_S3,
+    endpoint_url=ENDPOINT_SUP_S3,
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
     config=Config(signature_version='s3v4'),
